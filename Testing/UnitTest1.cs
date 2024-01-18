52cce24978f12595c9e841c54b642d5e0622199f
Justyn Fox <jjfox@bsu.edu>
2024-01-17 23:56:17 -0500

    Build Testing structure

--- Testing/UnitTest1.cs
+++ Testing/UnitTest1.cs
@@ -1,9 +1,22 @@
 namespace Testing;
 
+
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Test1_Pass()
+    {
+
+    }
+
+    [Fact]
+    public void Test2_Pass()
+    {
+
+    }
+
+    [Fact]
+    public void Test3_Failure()
     {
 
     }
