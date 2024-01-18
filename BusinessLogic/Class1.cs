872c66ddeb7cdbb50463fcc355a0f3e899078926
Justyn Fox <jjfox@bsu.edu>
2024-01-17 23:32:14 -0500

    Build BusinessLogic class

--- BusinessLogic/Class1.cs
+++ BusinessLogic/Class1.cs
@@ -1,6 +1,20 @@
-﻿namespace BusinessLogic;
-
-public class Class1
+﻿namespace BusinessLogic
 {
+    public class FactorialService
+    {
+        public int CalcFact(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Must be a positive integer");
+            }
+
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            return n * CalcFact(n - 1);
+        }
+    }
 
 }
